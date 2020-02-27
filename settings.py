f3ed5bc18d24cb21ac469ecc3132950db57771ac
Nicolás Pinzón <andresnikolas@gmail.com>
2020-02-27 18:51:15 -0500

    finishing project...
    add start button, speed up objects (alien, bullet and ship).
    pending the level dificulty and high score, score and lives labels...

--- settings.py
+++ settings.py
@@ -3,6 +3,7 @@ import pygame
 
 class Settings:
     def __init__(self):
+        # Screen settings
         self.screen_width = 1200
         self.screen_heigth = 720
         self.bg_color = [0, 0, 0]
@@ -10,14 +11,27 @@ class Settings:
         self.bg_image = pygame.transform.scale(self.bg_image, (self.screen_width, self.screen_heigth))
         self.bg_image_rect = self.bg_image.get_rect()
         self.bg_image_rect.left, self.bg_image_rect.top = (0,0)
-        self.ship_speed = 1.5
+        # Ship settings        
         self.ship_limit = 3
-        self.bullet_speed = 2.0
+        #Bullet settings        
         self.bullet_width = 7
         self.bullet_height = 15
         self.bullet_color = (249, 166, 2)
         self.bullets_allowed = 5
+        # Alien settings        
+        self.fleet_alien_speed = 10        
+        self.speedup_scale = 2
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        # Initialize these values normally
+        self.ship_speed = 1.5
+        self.bullet_speed = 2.0
         self.alien_speed = 1.0
-        self.fleet_alien_speed = 10
         self.fleet_direction = 1 
-        
+
+    def increase_speed(self):
+        # Increase these attributtes according to the speedup_scale value 
+        self.ship_speed *= self.speedup_scale
+        self.bullet_speed *= self.speedup_scale
+        self.alien_speed *= self.speedup_scale
